201d54a0c948272e35892f177ee0b012eb2bd728
Lauro Cabral <Lauro199471@gmail.com>
2018-09-19 18:06:56 -0700

    Dynamic Graphing

--- Lesson 1/lesson1.m
+++ Lesson 1/lesson1.m
@@ -7,6 +7,11 @@ end
 data = csvread('data.csv');
 x = data(:,1);
 y = data(:,2);
+pixel_size = 50;
+rgb = "g";
+scatter(x,y,pixel_size,rgb,"filled");
+axis([0 130 0 130]);
+title("Linear Regression - Radient Descent");
 
 % ==== Initilaze Linear Regression Parameter =====
 N = length(data);
@@ -17,8 +22,11 @@ alpha = 0.01;
 fprintf("Running Radient Descent....\n");
 
 J_history = zeros(interaions,1);
+xLine = 40:1:60;
 
 for iter = 1 :interaions
+    disp(iter);
+    
     idx = mod(iter,100)+1;
     hypothesis = (x(idx) * m) + b;
     
@@ -28,20 +36,25 @@ for iter = 1 :interaions
     m = temp_m;
     b = temp_b;
     J_history(iter) = (1/(2*N)) * sum((hypothesis - y(idx)) ^ 2);
+    
+    % === Graph as it Calculates ====
+    if (mod(iter,2) == 0)
+      hold on
+      yLine = (xLine * m) + b;
+      p = plot(xLine,yLine,'color',rand(1,3));
+      set(p, 'LineWidth' , 1);
+      hold off
+      pause(0.001);
+    end
 end
 
-% ==== Graph ====
+% ==== Graph Final ====
 fprintf("Slope:%.3f\ny-interept:%.3f" , m , b);
 
 xLine = 1:1:100;
 yLine = (xLine * m) + b;
-pixel_size = 50;
-rgb = "g";
-scatter(x,y,pixel_size,rgb,"filled");
-axis([0 130 0 130]);
-title("Linear Regression - Radient Descent");
 
 hold on;
-
 p = plot(xLine,yLine,'color','red');
 set(p, 'LineWidth' , 5);
+hold off
