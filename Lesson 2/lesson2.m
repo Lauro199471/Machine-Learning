35f39afc2d475951b4ba2f9cc477ad40e45c250a
Lauro Cabral <Lauro199471@gmail.com>
2018-09-21 13:35:21 -0700

    finished lesson 2

--- Lesson 2/lesson2.m
+++ Lesson 2/lesson2.m
@@ -3,23 +3,21 @@ clear;
 for clc = 0:30
     disp(" ");
 end
-figure(1, 'position',[0,0,1000,1000]);
+
+figure(1, 'position',[0,0,1500,500]);
+axis([0 1000 0 50]);
+title("Linear Regression - Radient Descent",'fontsize',30);
 
 data = csvread('data.csv');
 x = data(:,1);
 y = data(:,2);
-pixel_size = 50;
-rgb = "g";
-scatter(x,y,pixel_size,rgb,"filled");
-axis([0 130 0 130]);
-title("Linear Regression - Radient Descent");
 
 % ==== Initilaze Linear Regression Parameter =====
 N = length(data);
 m = 0;
 b = 0;
 interaions = 1500;
-alpha = 0.01;
+alpha = 0.0001;
 fprintf("Running Radient Descent....\n");
 
 J_history = zeros(interaions,1);
@@ -35,26 +33,20 @@ for iter = 1 :interaions
 
     m = temp_m;
     b = temp_b;
+    
     J_history(iter) = (1/(2*N)) * sum((hypothesis - y(idx)) ^ 2);
     
-    % === Graph as it Calculates ====
-    if (mod(iter,2) == 0)
-      hold on
-      yLine = (xLine * m) + b;
-      p = plot(xLine,yLine,'color',rand(1,3));
-      set(p, 'LineWidth' , 1);
-      hold off
-      pause(0.001);
-    end
 end
 
 % ==== Graph Final ====
 fprintf("Slope:%.3f\ny-interept:%.3f" , m , b);
 
-xLine = 1:1:100;
-yLine = (xLine * m) + b;
+xLine = 1:1:length(J_history);
+yLine = J_history;
 
 hold on;
 p = plot(xLine,yLine,'color','red');
-set(p, 'LineWidth' , 5);
+set(p, 'LineWidth' , 3);
+annotation('textbox', [0.35,0.75,0.5,0.1], 'string', 'Alpha = 0.0001',
+           'FontSize',50);
 hold off
