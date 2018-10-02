41ed5c11aa0f85a5d19d7d9273aadd45569c19fe
Lauro Cabral <Lauro199471@gmail.com>
2018-10-01 23:08:17 -0700

    Finished Lesson 4

--- Lesson 4/lesson4.m
+++ Lesson 4/lesson4.m
@@ -4,50 +4,19 @@ for clc = 0:30
     disp(" ");
 end
 
-% ======== Transpose Function =========
-function B = MyTranspose(A)
-[r, c, d] = size(A);
-B = zeros(c,r);
-for row = 1:r
-    for col = 1:c
-        B(col,row) = A(row,col);
-    end
-end
-endfunction
+% X(theta) = y
+data = csvread('data.csv');
+X = data(:,1:4); % X Matrix
+y = data(:,5);   % y vector
+oneCol = (ones(size(X),1)); % Xsize x 1 of 1's
+X = [oneCol,X];
+Xt = transpose(X);
 
-%========== Main ===========
-oriImage = imread('ClownFish.png');
-imshow(oriImage);
+% theta = ((Xt * X) ^ -1 ) Xt * y
+a = inverse(Xt * X);
+b = Xt * y;
 
-[row,col,depth] = size(oriImage);
+theta = a * b
 
-% Get the Red Channel , Blue Channel, Green Channel
-R = oriImage(:,:,1); 
-G = oriImage(:,:,2); 
-B = oriImage(:,:,3); 
-
-figure,imshow(R);
-figure,imshow(G);
-figure,imshow(B);
-
-% Combine RGB to recreate Original
-RGB = cat(3,R,G,B);
-figure,imshow(RGB);
-
-% Transpose Image
-% 1) Rotates the matrix by 90 degrees
-% 2) Change Rows to Columns and Columns to Rows( basically mirrors )
-     
-transR = transpose(R);
-transG = transpose(G);
-transB = MyTranspose(B);
-
-transRGB = cat(3,transR,transG,transB);
-figure,imshow(transRGB);
-
-
-
-
-
-
-
+newy = X * theta
+y
