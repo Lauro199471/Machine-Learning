5fe0f5dca347371d4e67a66856a60295c040ce72
Lauro Cabral <Lauro199471@gmail.com>
2018-10-22 21:15:22 -0700

    Updated plot feature and readme

--- Iris_example/Iris_example.m
+++ Iris_example/Iris_example.m
@@ -5,61 +5,12 @@
 % 'g' - green
 % 'b' - blue
 % 'w' - white
-% 'k' - black
-% sepal_length  sepal_width  petal_length petal_width species
-%      1             2            3            4         5
-clear;
-for clc = 0:30
-    disp(" ");
-end
+% 'k' - black");
 % ========== Main ===========
 % Load and Prepare Data
-%data = csvread('iris.csv');
-%plotFeature(data,4,3,'Petal Width','Petal Length','Petal Leaves');
-%clear data;
+data = csvread('iris.csv');
+splitMap = get_potential_splits(data);
+petal_width = 4;
 
-data = csvread('iris-sample.csv');
-mostLikey = classify_data(data);
-
-if( mostLikey == 0)
-    disp("Iris-Setosa");
-elseif( mostLikey == 1)
-    disp("Iris-versicolor");
-elseif( mostLikey == 2)
-    disp("Iris-virginica");
-end
-
-%featureMatrix = data(:, 1:size(data,2)-1);
-
-%labelVector = data(:,size(data,2));
-
-% Plot Data
-
-%testSplit(featureMatrix , labelVector); % Test 
-
-%potential_splits =  get_potential_splits(data);
-%[best_split_column best_split_value] = determine_best_split(data , potential_splits);
-
-%for k = keys(potential_splits)
-%  thekey = k{1}; % The curly braces are the key, so to speak
-%   h = potential_splits(thekey);
-%end
-
-%h
-
-%plotFeature(featureMatrix,labelVector);
-%vline(potential_splits('4'),'k');
-
-
-%hline(petal_width_splits ,'k');
-%entropy = calculate_entropy(labelVector(51:150));
-
-
-%overal_entropy = calculate_Overall_entropy(data_below , data_above);
-
-
-%plotFeature(featureMatrix,labelVector);
-%hold on;
-%vline( 1.05 ,'k');
-
+plotFeature(data,4,3,'Petal Width','Petal Length','Petal Leaves');
 
--- Iris_example/get_potential_splits.m
+++ Iris_example/get_potential_splits.m
@@ -1,13 +1,12 @@
-function containerSplit =  get_potential_splits(data)
-    containerSplit = containers.Map;
-
+function hashMAP =  get_potential_splits(data)
     nFeatures = size(data,2) - 1; % -1 because we dont need label
-    for(column_index = 1 : nFeatures)
+
+    for(column_index = 3 : nFeatures) % loop through rows
         column_value = data( : , column_index);
         [uniqueValues uniqueValues_counts] = uniqueness(column_value);
 
         for(row_index_uniqueValues = 1 : length(uniqueValues))
-            if(row_index_uniqueValues > 1)
+            if(row_index_uniqueValues > 1) % skip one cause no avarage
                 currentValue = uniqueValues(row_index_uniqueValues);
                 previosValue = uniqueValues(row_index_uniqueValues - 1);
 
@@ -16,7 +15,7 @@ function containerSplit =  get_potential_splits(data)
             end
         end
 
-        containerSplit(int2str(column_index)) = split_values;
+        hashMAP.(int2str(column_index)) = split_values;
     end
 
 return
