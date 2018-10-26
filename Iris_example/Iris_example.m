6f5b1764f50324c929a1db54e104c10705377297
Lauro Cabral <Lauro199471@gmail.com>
2018-10-25 23:15:59 -0700

    Almost done with Main Algothrim

--- Iris_example/Iris_example.m
+++ Iris_example/Iris_example.m
@@ -1,3 +1,31 @@
 data = csvread('iris.csv');
 
-decision_tree_algorithm(data, 0);
+data = data(1:100 , : );
+
+potential_splits =  get_potential_splits(data);
+[best_feature_value best_split_value] = determine_best_split(data , potential_splits);
+[data_above data_below] = split_data(data , best_feature_value , best_split_value );
+
+best_feature_valueS = ' ';
+if( best_feature_value == 1)
+    best_feature_valueS = "sepal_length";
+elseif( best_feature_value == 2)
+    best_feature_valueS = "sepal_width";
+elseif( best_feature_value == 3)
+    best_feature_valueS = "petal_length";
+elseif( best_feature_value == 4)
+    best_feature_valueS = "petal_width";
+end
+
+question = [ best_feature_valueS , ' <= ' , num2str(best_split_value) ];
+disp(question);
+
+
+% Find Answers(recursion)
+if(check_purity(data_below) == 1)
+  disp('This is Pure');
+end
+
+if(check_purity(data_above) == 1)
+  disp('This is Pure');
+end
--- Iris_example/decision_tree_algorithm.m
+++ Iris_example/decision_tree_algorithm.m
@@ -1,16 +1,37 @@
-function classification = decision_tree_algorithm(data)
+function [stringLabel subTree] = decision_tree_algorithm(data)
 
-    % Base Case
+    % Is Data pure?
     if(check_purity(data) == 1)
         disp('This is Pure');
-        classification = classification = classify_data(data)
+        classification = classify_data(data)
+        
+        stringLabel = " ";
+        if( classification == 0)
+            stringLabel = "Iris-Setosa";
+        elseif( classification == 1)
+            stringLabel = "Iris-versicolor";
+        elseif( classification == 2)
+            stringLabel = "Iris-virginica";
+        end
+    
+    % If not then find pure
     else
         disp('This is not pure');
 
         potential_splits =  get_potential_splits(data);
         [best_feature_value best_split_value] = determine_best_split(data , potential_splits);
         [data_above data_below] = split_data(data , best_feature_value , best_split_value );
-        classification = data_below
+
+        question = [ num2str(best_feature_value) , ' <= ' , num2str(best_split_value) ];
+        
+        % Find Answers(recursion)
+        [yes_answers idc]= decision_tree_algorithm(data_below);
+        [no_answers IDC]= decision_tree_algorithm(data_above);
+        
+        StringYesNo = [yes_answers,' ',no_answers];
+
+        subTree = [question StringYesNo];
+
     end
 
 end
