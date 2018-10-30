05ed983d65cd26c64bf6a8476e3c25e4dac78a4d
Lauro Cabral <Lauro199471@gmail.com>
2018-10-29 22:08:10 -0700

    Started Golf

--- Chapter 1 - Decision Trees/Iris_example/Iris_example.m
+++ Chapter 1 - Decision Trees/Iris_example/Iris_example.m
@@ -8,73 +8,18 @@ end
 
 data = csvread('iris.csv');
 
-% ===== Testing =====
 
-examplePoint = [5.1,3.5,1.4,0.2,0]
+Tree = decision_tree_algorithm(data);
+Tree
 
-% Find Feature and where to cut off
-potential_splits =  get_potential_splits(data);
-[best_feature_value best_split_value] = determine_best_split(data , potential_splits);
-[data_above data_below] = split_data(data , best_feature_value , best_split_value );
+%A = cell2struct ({"Peter", "Hannah", "Robert";
+%                   185, 170, 168} ,
+%                 {"Name","Height"}, 1);
 
+%B = {"Lauro" , [19 , 94]};
+%C = {"Raider", [20 , 05]};
+%B = [B ; C]
 
-question = [ '? ' , num2str(best_feature_value) , ' <= ' , num2str(best_split_value) ];
-
-% Find Answers(recursion)
-yes_answers_label = "0";
-no_answers_label = "? 3 <= 4.85";
- 
-C = {question , {yes_answers_label , no_answers_label} };
-
-noAnsStr = C{1,2,1}{2};
-%noAnsStrSplit = strsplit(noAnsStr);
-
-yesAnsStr = C{1,2,1}{1};
-%yesAnsStrSplit = strsplit(yesAnsStr);
-
-
-questionList = strsplit(C{1}(1,:));
-
-disp('Question');
-disp(question);
-
-disp('Yes:');
-disp(yesAnsStr);
-
-disp('No:');
-disp(noAnsStr);
-
-questionSplitValueStr = questionList{1,4};
-questionFeatuValueStr = questionList{1,2};
-
-questionSplitValue = str2double(questionSplitValueStr);
-questionFeatuValueStr = str2num(questionFeatuValueStr);
-
-if(questionList{1,1} == '?')
-    
-    exFeaValue = str2num(questionList{1,2});
-    if(examplePoint(1 , exFeaValue) <= questionSplitValue)
-        answer = yesAnsStr;
-    else
-        answer = noAnsStr;
-    end
-end
-
-answerSplit = strsplit(answer);
-
-if(answerSplit{1,1} == '?')
-    % Repeat
-    disp("Here");
-else
-    answerNum = str2num(answer);
-    if( answerNum == 0)
-        subTree = "Iris-Setosa"
-    elseif( answerNum == 1)
-        subTree = "Iris-versicolor"
-    elseif( answerNum == 2)
-        subTree = "Iris-virginica"
-    end
-end
-
-
-
+%C = [B ; C]
+%counter = 1;
+%D{counter,1} = "Paco"
--- Chapter 1 - Decision Trees/Iris_example/decision_tree_algorithm.m
+++ Chapter 1 - Decision Trees/Iris_example/decision_tree_algorithm.m
@@ -1,50 +1,34 @@
-function subTree = decision_tree_algorithm(data)
-
-    subTree = " ";
-
+function tree = decision_tree_algorithm(data)
+    global counterLevel = 0
     % Is Data pure?
     if(check_purity(data) == 1)
         % return Label Class
-        classification = classify_data(data);
-        
-        if( classification == 0)
-            subTree = "Iris-Setosa";
-        elseif( classification == 1)
-            subTree = "Iris-versicolor";
-        elseif( classification == 2)
-            subTree = "Iris-virginica";
-        end
-    
-    % If not then find pure
+        tree = num2str(classify_data(data));
+
+    % else return Tree
     else
-        % Find Feature and where to cut off
         potential_splits =  get_potential_splits(data);
         [best_feature_value best_split_value] = determine_best_split(data , potential_splits);
         [data_above data_below] = split_data(data , best_feature_value , best_split_value );
 
-        % sepal_length  sepal_width  petal_length petal_width species
-        %      1             2            3            4         5
-        
-        bestFeature ="";
-        if(best_feature_value == 1)
-            bestFeature = 'Sepal Length';
-        elseif(best_feature_value == 2)
-            bestFeature = 'Sepal Width';
-        elseif(best_feature_value == 3)
-            bestFeature = 'Petal Length';
-        elseif(best_feature_value == 4)
-            bestFeature = 'Petal Width';
+        question = ['? ' ,num2str(best_feature_value) , ' <= ' , num2str(best_split_value) ];
+        questionNode = {question , ["" , ""]};
+
+        yes_answers_label = getSubTree(data_below)
+        no_answers_label  = getSubTree(data_above)
+
+        questionNode{1,2,1}{1} = yes_answers_label;
+        questionNode{1,2,1}{2} = no_answers_label;
+
+        % If there is Question Node Repeat
+        yesSplit = strsplit(yes_answers_label)
+        noSplit = strsplit(no_answers_label)
+        if(yesSplit{1,1} == '?')
+            disp('Yes got a question');
+        elseif(noSplit{1,1} == '?')
+            disp('No got a question');
         end
-
-        question = [ bestFeature , ' <= ' , num2str(best_split_value) ];
         
-        % Find Answers(recursion)
-        yes_answers_label = decision_tree_algorithm(data_below);
-        no_answers_label  = decision_tree_algorithm(data_above);
-                
-        StringYesNo = [yes_answers_label," , ",no_answers_label];
-        subTree = [question , " ? [" , StringYesNo "]"];
-
+        tree = questionNode;
     end
-
 end
