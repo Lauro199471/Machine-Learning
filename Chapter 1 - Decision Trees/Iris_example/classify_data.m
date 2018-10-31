e9c7802d7886af909f002bbfe87d40b9fc4e28a3
Lauro Cabral <Lauro199471@gmail.com>
2018-10-30 22:43:49 -0700

    Started Python ML

--- Chapter 1 - Decision Trees/Iris_example/classify_data.m
+++
@@ -1,7 +0,0 @@
-function classification = classify_data(data)
-  labelVector = data(:,size(data,2));
-  [uniqueClasses uniqueClasses_counts] = uniqueness(labelVector);
-  [valueMax classificationIndex] = max(uniqueClasses_counts);
-  classification = uniqueClasses(classificationIndex);
-  return
-end
